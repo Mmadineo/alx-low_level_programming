ecafd9d67744722f9ca7ae6554ffad818d06eabf
Mmadineo <mmadineo.seleke@gmail.com>
2023-02-20 06:48:13 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,16 +6,17 @@
  */
 int main(void)
 {
-	int n;
+	int a;
 
-	for (n = 48; n < 58; n++)
+	for (a = 0; a < 100; a++){
 
-		putchar(n);
-		if (n != 57)
-		{
+		putchar(a +'0');
+		if i(a < 99) {
+		
 				putchar(',');
 				putchar(' ');
 		}
+	}
 		putchar('\n');
 
 		return (0);
