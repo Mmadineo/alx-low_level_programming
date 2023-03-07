a7aaa62dff33faf903e11843b8e7016d38bd92c1
Mmadineo <mmadineo.seleke@gmail.com>
2023-03-06 19:11:52 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _memcoy - a function that copies memory area
+ * _memcpy - a function that copies memory area
  * @dest: memory where is stored
  * @src: memory where is copied
- * @n number of bytes
+ * @n: number of bytes
  *
  * Return: copied memory with n byted changed
  */
