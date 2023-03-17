d69c70a48c41e3f28de562185c58b6f95526cd9f
Mmadineo <mmadineo.seleke@gmail.com>
2023-03-17 04:23:45 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,12 +10,12 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *i;
 
-	ptr = malloc(b);
+	i = malloc(b);
 
-	if (ptr == NULL)
+	if (i == NULL)
 		exit(98);
 
-	return (ptr);
+	return (i);
 }
