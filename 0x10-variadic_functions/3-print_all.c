72276edd85335beb7a0aedd5a45e52e406543ab3
Mmadineo <mmadineo.seleke@gmail.com>
2023-03-23 08:03:56 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,7 +14,6 @@ void print_all(const char * const format, ...)
 	va_list list;
 
 	va_start(list, format);
-
 	if (format)
 	{
 		while (format[i])
@@ -24,34 +23,26 @@ void print_all(const char * const format, ...)
 				case 'c':
 					printf("%s%c", sep, va_arg(list, int));
 					break;
-
 				case 'i':
 					printf("%s%d", sep, va_arg(list, int));
 					break;
-
 				case 'f':
 					printf("%s%f", sep, va_arg(list, double));
 					break;
-
 				case 's':
 					str = va_arg(list, char *);
-
 					if (!str)
 						str = "(nil)";
-
 					printf("%s%s", sep, str);
 					break;
-
 				default:
 					i++;
 					continue;
 			}
-
 			sep = ", ";
 			i++;
 		}
 	}
-
 	printf("\n");
 	va_end(list);
 }
