2f4dcc45949b4ba81ae5e93263838357f1cfe07d
Mmadineo <mmadineo.seleke@gmail.com>
2023-04-04 06:24:43 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#inclde "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked list
+ * @h : linked list of type listint_t to traverse
+ *
+ * Return : number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t mad = 0;
+
+	while (h)
+	{
+		mad++;
+		h = h->next;
+	}
+
+	return (mad);
+}
