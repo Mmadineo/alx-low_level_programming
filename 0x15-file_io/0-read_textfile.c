094b460067ba6c329607bccf6b4fa378adcee885
Mmadineo <mmadineo.seleke@gmail.com>
2023-05-08 06:23:51 -0700

    Task 0- Tread lightly, she's near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,26 +6,26 @@
  * @filename: Read text file
  * @letters: Number of letters to be read
  *
- * Return: b - actual bytes read and peinted
+ * Return: w - actual bytes read and peinted
  *	0 - filename is NULL
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
-	ssize_t filedescriptor;
-	ssize_t b;
-	ssize_t m;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	filedescriptor = open(filename, O_RDONLY);
-	if (filedescriptor == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	m = read(filedescriptor, buffer, letters);
-	b = write(STDOUT_FILENO, buffer, m);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	free(buffer);
-	close(filedescriptor);
-	return (b);
+	free(buf);
+	close(fd);
+	return (w);
 }
