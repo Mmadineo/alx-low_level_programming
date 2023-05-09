1eeb4d096a1c30ac6f68affdb2232a74b63b2ec5
Mmadineo <mmadineo.seleke@gmail.com>
2023-05-09 02:39:42 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,14 +14,11 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
-
 	for (a = 0; b[a]; a++)
 	{
 		if (b[a] < '0' || b[a] > '1')
 			return (0);
 		dec_value = 2 * dec_value + (b[a] - '0');
 	}
-
 	return (dec_value);
 }
-
