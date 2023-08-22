aae1ba160c2494167b97bf2285191af4a0f66ec9
Mmadineo <mmadineo.seleke@gmail.com>
2023-08-22 04:04:48 -0700

    Task 2 correction

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,14 +1,40 @@
+#include <stdio.h>
+
 int add(int a, int b)
-	return (a+b);
+{
+	return (a + b);
+}
 
 int sub(int a, int b)
-	return (a – b);
+{
+	return (a - b);
+}
 
 int mul(int a, int b)
+{
 	return (a * b);
+}
 
-int div(int a, int b)
+int divide(int a, int b)
+{
 	return (a / b);
+}
 
 int mod(int a, int b)
+{
 	return (a % b);
+}
+
+int main()
+{
+        int x = 10;
+        int y = 5;
+
+        printf("Addition: %d\n", add(x, y));
+        printf("Subtraction: %d\n", sub(x, y));
+        printf("Multiplication: %d\n", mul(x, y));
+	printf("Division: %d\n", divide(x, y));
+        printf("Modulus: %d'n", mod(x, y));
+
+        return (0);
+}
